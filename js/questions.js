5cafcd697b73ebfb6948f7b1bd3296a934d4199b
Rammah Mohamed <Rammahmohamed.rm@gmail.com>
2023-04-14 10:42:36 +0200

    upload

---
+++ js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+    num: 1,
+    answer: "JavaScript is a scripting language used to make the website interactive",
+    quiz: "What is JavaScript?",
+    options : [
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is an assembly language used to make the website interactive",
+      "JavaScript is a compiled language used to make the website interactive",
+      "None of the mentioned"
+    ]
+  },
+  {
+    num: 2,
+    answer: "JavaScript is an Object-Based language",
+    quiz: "Which of the following is correct about JavaScript?",
+    options : [
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Based language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language"
+    ]
+  },
+  {
+    num: 3,
+    answer: "It is an ordered list of values",
+    quiz: "Arrays in JavaScript are defined by which of the following statements?",
+    options : [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions"
+    ]
+  },
+  {
+    num: 4,
+    answer: "Allows JavaScript code to alter the behaviour of windows",
+    quiz: "Why event handlers is needed in JS?",
+    options : [
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences",
+      "Allows JavaScript code to alter the behaviour of windows"
+    ]
+  },
+  {
+    num: 5,
+    answer: "Division by zero",
+    quiz: "Which of the following is not an error in JavaScript?",
+    options : [
+      "Missing of Bracket",
+      "Division by zero",
+      "Syntax error",
+      "Missing of semicolons"
+    ]
+  }
+]
